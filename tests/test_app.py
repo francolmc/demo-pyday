6ac181d57cdb29471da7b60c7122e0ae65526d45
Franco Morales Castro <franco.morales@outlook.com>
2024-06-12 01:27:13 -0400

    feat: basic unit tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,26 +1,13 @@
-import pytest
-from application import create_app
-from application.extensions import db
+import json
 from application.tasks.models import Task
 
-@pytest.fixture
-def client():
-    app = create_app(testing=True)
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        with app.app_context():
-            db.create_all()
-            yield client
-        with app.app_context():
-            db.drop_all()
-
-def test_get_tasks(client):
-    response = client.get("/api/tasks")
+def test_get_tasks(test_client, test_database):
+    response = test_client.get("/api/tasks")
     assert response.status_code == 200
     assert response.get_json() == []
 
-def test_create_task(client):
-    response = client.post("/api/tasks", json={
+def test_create_task(test_client, test_database):
+    response = test_client.post("/api/tasks", json={
         "title": "Test Task",
         "description": "This is a test task"
     })
@@ -30,12 +17,12 @@ def test_create_task(client):
     assert data["task"]["description"] == "This is a test task"
     assert not data["task"]["completed"]
 
-def test_complete_task(client):
-    client.post("/api/tasks", json={
+def test_complete_task(test_client):
+    test_client.post("/api/tasks", json={
         "title": "Test Task",
         "description": "This is a test task"
     })
-    response = client.post("/api/tasks/1/complete")
+    response = test_client.post("/api/tasks/1/complete")
     assert response.status_code == 200
     data = response.get_json()
     assert data["task"]["completed"]
