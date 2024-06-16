2db11a20e3bd971b682b243f47ca6b3a34fb20f6
Franco Morales Castro <franco.morales@outlook.com>
2024-06-15 22:25:09 -0400

    feat: cambio de base de datos a turso

--- application/config.py
+++ application/config.py
@@ -3,10 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+TURSO_DATABASE_URL = os.environ.get("TURSO_DATABASE_URL")
+TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
+
+CONNECTION_STRING = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_CONNECTION_STRING")
+    SQLALCHEMY_DATABASE_URI = CONNECTION_STRING
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_TEST_CONNECTION_STRING")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_CONNECTION_STRING")
--- application/tasks/models.py
+++ application/tasks/models.py
@@ -6,6 +6,11 @@ class Task(db.Model):
     description = db.Column(db.String(200))
     completed = db.Column(db.Boolean, default=False)
 
+    def __init__(self, title: str, description: str) -> None:
+        super().__init__()
+        self.title = title
+        self.description = description
+
     def complete(self):
         self.completed = True
 
